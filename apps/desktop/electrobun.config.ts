487f539442407499b88ef2b7d7c50672baea29de
tekibo <tekibohelp@gmail.com>
2026-08-31 01:24:50 +0530

    reverse patch

--- apps/desktop/electrobun.config.ts
+++ apps/desktop/electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
   app: {
     name: "NoteWordy",
     identifier: "com.tekibo.notewordy",
-    version: "0.0.2",
+    version: "0.0.1",
   },
   release: {
     baseUrl: "https://github.com/tekibo/notewordy/releases/latest/download",
